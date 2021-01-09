406c2b510cecce6b732075008f77c86006b8ad7c
mswgen <54498184+mswgen@users.noreply.github.com>
2021-01-09 12:34:46 +0900

    버그 픽스

--- index.js
+++ index.js
@@ -33,11 +33,21 @@ client.on('message', async message => {
     }
     if (message.channel.id == '796291747860840468') {
         await db.updateOne({_id: message.author.id}, {
-            $set: {chats: {mine: (await db.findOne({_id: message.author.id})).chats.mine + 1}}
+            $set: {
+                chats: {
+                    hill: (await db.findOne({_id: message.author.id})).chats.hill,
+                    mine: (await db.findOne({_id: message.author.id})).chats.mine + 1
+                }
+            }
         });
     } else if (message.channel.id == '796293274016153640') {
         await db.updateOne({_id: message.author.id}, {
-            $set: {chats: {hill: (await db.findOne({_id: message.author.id})).chats.hill + 1}}
+            $set: {
+                chats: {
+                    hill: (await db.findOne({_id: message.author.id})).chats.hill + 1,
+                    mine: (await db.findOne({_id: message.author.id})).chats.mine
+                }
+            }
         });
     }
 });
